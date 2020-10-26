4e0d5239080822034ca8f3402af7d209c39f7f49
Nakharin Donsuypae <nakharin.donsuypae@outlook.com>
2020-10-26 18:03:47 +0100

    Added tests and documentation

---
+++ src/test/kotlin/de/novatec/betting/game/scoreboard/ScoreboardServiceUnitTest.kt
@@ -0,0 +1,42 @@
+package de.novatec.betting.game.scoreboard
+
+import de.novatec.betting.game.matchday.matchDayOverviewTf
+import de.novatec.betting.game.model.ScoreboardTeam
+import de.novatec.betting.game.model.Team
+import de.novatec.betting.game.model.Teams
+import de.novatec.betting.game.openliga.OpenLigaAccessor
+import de.novatec.betting.game.openliga.model.OLScoreboardTeam
+import de.novatec.betting.game.openliga.model.OLTeam
+import de.novatec.betting.game.scoreboard.tf.ScoreboardTf
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkClass
+import io.mockk.verify
+import org.assertj.core.api.Assertions
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.junit.jupiter.api.Test
+import utils.classification.UnitTest
+
+val openLigaAccessor: OpenLigaAccessor = mockk()
+val scoreboardTf: ScoreboardTf = mockk()
+
+@UnitTest
+class ScoreboardServiceUnitTest {
+
+    private val service = ScoreboardService(openLigaAccessor, scoreboardTf)
+
+    @Test
+    fun `get scoreboard for a season`() {
+
+        val olScoreboardTeam = listOf(mockkClass(OLScoreboardTeam::class), mockkClass(OLScoreboardTeam::class))
+        val scoreboardTeam = listOf(mockkClass(ScoreboardTeam::class))
+
+        every { openLigaAccessor.getScoreboard("2019") } returns olScoreboardTeam
+        every { scoreboardTf.oLScoreboardToScoreboard(olScoreboardTeam) } returns scoreboardTeam
+
+        service.getScoreboard("2019")
+
+        verify { scoreboardTf.oLScoreboardToScoreboard(olScoreboardTeam) }
+    }
+
+}
